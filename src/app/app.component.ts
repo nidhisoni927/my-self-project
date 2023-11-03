241ee9d81c1769b60c337551f6202d9b425e9cd0
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-03 18:22:19 +0530

    pipe in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,27 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-self-project';
-  users = [
-    {
-      name: 'nidhi',
-      age: 28,
-      address: 'pune'
-    },
-    {
-      name: 'jay',
-      age: 22,
-      address: 'indore'
-    },
-    {
-      name: 'ram',
-      age: 48,
-      address: 'india'
-    },
-    {
-      name: 'sangu',
-      age: 36,
-      address: 'mumbai'
-    }
 
-  ]
 }
