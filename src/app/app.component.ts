ff6af98e89901da1b38499c20ca26683bb46eca9
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-03 11:36:41 +0530

    add user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,6 @@
 import { Component } from '@angular/core';
-import {reduce} from "rxjs";
 
-const ALERTS: Alert[] = [
-  {
-    type: 'success',
-    message: 'This is an success alert',
-  },
-  {
-    type: 'info',
-    message: 'This is an info alert',
-  },
-  {
-    type: 'warning',
-    message: 'This is a warning alert',
-  },
-  {
-    type: 'danger',
-    message: 'This is a danger alert',
-  },
-  {
-    type: 'primary',
-    message: 'This is a primary alert',
-  },
-  {
-    type: 'secondary',
-    message: 'This is a secondary alert',
-  },
-  {
-    type: 'light',
-    message: 'This is a light alert',
-  },
-  {
-    type: 'dark',
-    message: 'This is a dark alert',
-  },
-];
 
-class Alert {
-}
 
 @Component({
   selector: 'app-root',
@@ -46,20 +9,7 @@ class Alert {
 })
 export class AppComponent {
   title = 'my-self-project';
-  public alerts: any;
 
-
-  constructor() {
-    this.reset();
-  }
-
-  close(alert: Alert) {
-    this.alerts.splice(this.alerts.indexOf(alert), 1);
-  }
-
-  reset() {
-    this.alerts = Array.from(ALERTS);
-  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UsersComponent } from './users/users.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    UsersComponent,
 
   ],
   imports: [
