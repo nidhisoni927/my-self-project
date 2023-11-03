721c109a8d2339559a31ec7fefc47170479d9afe
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-03 11:57:12 +0530

    user functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-self-project';
-
+  data="nidhi soni"
 }
 
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit,Input} from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
+
+  @Input() hero: any
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
 
 }
