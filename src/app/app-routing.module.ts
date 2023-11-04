039332c6300faea0de10b179f10729ad3290e51c
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-04 09:45:06 +0530

    404 page not found done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from "./user/user.component";
 import {AdminComponent} from "./admin/admin.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 
 const routes: Routes = [
@@ -12,6 +13,11 @@ const routes: Routes = [
   {
     path:'admin',
     component:AdminComponent
+  },
+  {
+    path:'**',
+    component:PageNotFoundComponent
+
   }
 
 
