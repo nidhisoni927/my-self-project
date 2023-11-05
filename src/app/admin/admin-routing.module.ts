9c24df5270ebf187fdcc056c01c1b5ba649a172f
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-05 23:08:08 +0530

    routing installation

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component'
+import {ListComponent} from './list/list.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'login' ,component:LoginComponent},
+  {path: 'list' , component:ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-
+import{AdminModule}from './admin/admin.module'
 import { AppRoutingModule } from './app-routing.module';
 import {AppComponent } from './app.component'
 
@@ -15,7 +15,7 @@ import {AppComponent } from './app.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+     AdminModule
 
   ],
   providers: [],
