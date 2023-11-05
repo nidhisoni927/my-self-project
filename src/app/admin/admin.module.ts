e6b13527b23160d0a7f8d02c08d778000433e0c4
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-05 22:49:16 +0530

    setup done

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { LoginComponent } from './login/login.component';
 import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
+    LoginComponent,
     ListComponent
   ],
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {UsersModule} from "./users/users.module";
-import { LoginComponent } from './admi/login/login.component';
+import {AppComponent } from './app.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UsersModule,
+
 
   ],
   providers: [],
