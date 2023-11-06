7573c7d116bc2120643350ee84eb26d489a40463
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-06 12:41:38 +0530

    some changes in routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
+import {AdminModule} from './admin/admin.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import {AppComponent } from './app.component'
@@ -16,7 +16,7 @@ import {AppComponent } from './app.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    AdminModule
 
 
   ],
