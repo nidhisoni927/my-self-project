b89be7a270480a4f7a31e9f0a0c9922a1b4ef79d
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-07 17:47:48 +0530

    Angular template driven form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {Error404Component} from "./components/error404/error404.component";
-import {AdminComponent} from "./admin/admin.component";
-import {UserComponent} from "./user/user.component";
-import {UserLoginComponent} from "./user/user-login/user-login.component";
-import {UserListComponent} from "./user/user-list/user-list.component";
-import {AdminLoginComponent} from "./admin/admin-login/admin-login.component";
-import {AdminListComponent} from "./admin/admin-list/admin-list.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/user/login', pathMatch: 'full' },
-  {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
-  },
-  {
-    path: 'user',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
-  },
-  { path: '**', component: Error404Component }
+
+
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
-
 import { BrowserModule } from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-
 import { AppRoutingModule } from './app-routing.module';
-import { Error404Component } from './components/error404/error404.component'
 import {AppComponent} from "./app.component";
-
-
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
